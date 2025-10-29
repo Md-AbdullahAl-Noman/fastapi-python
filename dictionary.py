9e3d273c3f88985d7f402d689a24fb6e3928bcf1
Md-AbdullahAl-Noman <abdullahnoman1414@gmail.com>
2025-10-29 02:24:43 +0100

    Add example demonstrating dictionary key retrieval in Python

--- dictionary.py
+++ dictionary.py
@@ -9,6 +9,9 @@ my_dict1={
     "city":"Copenhagen"
 }
 print(my_dict1)
+#get the keys only
+for key in my_dict1.keys():#or values() to get only values of the dictionaries
+    print("key:",key)
 #accessing value using key
 print(my_dict1["name"])
 #adding new key value pair
